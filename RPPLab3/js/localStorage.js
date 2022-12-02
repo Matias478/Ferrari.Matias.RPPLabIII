fcbcccd7a6f0e833da2d1cff8e3af6d1e3f2e227
Matias Ferrari <67980562+Matias478@users.noreply.github.com>
2022-12-02 20:11:04 -0300

    Delete RPPLab3 directory

--- RPPLab3/js/localStorage.js
+++
@@ -1,7 +0,0 @@
-export function getLocalStorageData(storageKey) {
-    return JSON.parse(localStorage.getItem(storageKey)) || [];
-}
-
-export function setLocalStorageData(storageKey, lista) {
-    localStorage.setItem(storageKey, JSON.stringify(lista));
-}
